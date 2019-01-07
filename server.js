fad8ae8078ccfa3da86ef8d276ca06d980638740
Shafi Akhtar <shafi@Shafis-MacBook-Air.local>
2019-01-07 23:48:21 +0530

    last

--- server.js
+++ server.js
@@ -1,18 +1,57 @@
 const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
 var app=express();
+hbs.registerPartials(__dirname+'/views/partial')
+app.set('view engine', 'hbs');
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now},${req.method},${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log('unable to append server.log');
+        }
+    });
+   next();
+});
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname+'/public'));
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
 app.get('/', (req,res)=>{
-    res.send({
-        name: 'shafi',
-        likes: [
-            'football',
-            'gym'
-        ]
+    // res.send({
+    //     name: 'shafi',
+    //     likes: [
+    //         'football',
+    //         'gym'
+    //     ]
+    // });
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to Home Page',
+        //currentYear: new Date().getFullYear()
+
     });
 });
 app.get('/about',(req,res)=>{
-    res.send('About Page');
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+       // currentYear: new Date().getFullYear()
+    });
 
 });
 app.get('/bad',(req,res)=>{
-    errorMessage: 'Unable to handle request'
-});app.listen(8000);
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+app.listen(8000,()=>{
+    console.log('Connection Establish Successfully');
+});
